d54b51900a51df0cfabb8309b4f61c4700b76ad6
jessie <jessie@webglsoft.com>
2023-06-20 16:37:07 +0800

    fix: XXX

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,12 +1,26 @@
 module.exports = {
-  // ignores: [(commit) => commit.includes('init')],
+  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-    'subject-case': [0],
+    'type-empty': [0],
+    'subject-case': [0, 'never'],
   },
 }
+
+// module.exports = {
+//   // ignores: [(commit) => commit.includes('init')],
+//   extends: ['@commitlint/config-conventional'],
+//   rules: {
+//     'type-enum': [2, 'always',['feat','fix']],
+//     'body-leading-blank': [2, 'always'],
+//     'footer-leading-blank': [1, 'always'],
+//     'header-max-length': [2, 'always', 108],
+//     'subject-empty': [2, 'never'],
+//     'type-empty': [0],
+//     'subject-case': [0, 'never'],
+//   },
+// }
